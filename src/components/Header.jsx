1662c5241f5ddd9bc99a1472e947e7a8a83be7c6
Ibrahiim10 <ustadibrahim00@gmail.com>
2025-06-27 17:47:33 +0300

    Setting Up Dropdown Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const avatarUrl = null;
+  // 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D';
+
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -48,24 +55,71 @@ const Header = () => {
           {/* right */}
           <div className="flex items-center space-x-4">
             {/* profile */}
-            <div>
-              <span>Hello, Ibra</span>
-            </div>
-            {/* buttons */}
-            <div className=" hidden sm:flex sm:items-center sm:space-x-4">
-              <Link
-                to="/signin"
-                className="inline-flex items-center justify-center px-4 py-2 border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-              >
-                Sign In
-              </Link>
-              <Link
-                to="/signup"
-                className="hidden sm:inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-md text-orange-600 bg-white border border-orange-600 hover:bg-orange-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-              >
-                Sign Up
-              </Link>
-            </div>
+
+            {isLoggedIn ? (
+              <>
+                <div className="text-sm text-gray-700">
+                  <span>Hello, Ibra</span>
+                </div>
+
+                <div className="relative">
+                  <button
+                    className="flex items-center justify-center h-8 w-8 rounded-full bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+                    onMouseEnter={() => setIsDropdownOpen(true)}
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  >
+                    {
+                      /* Profile Avatar */
+                      avatarUrl ? (
+                        <img
+                          src={avatarUrl}
+                          alt="Avatar"
+                          className="h-8 w-8 rounded-full"
+                        />
+                      ) : (
+                        <span className="text-gray-500">
+                          <FaUser className="text-gray-600" />
+                        </span>
+                      )
+                    }
+                  </button>
+                  {/* dropdown menu  */}
+                  {isDropdownOpen && (
+                    <div
+                      className="absolute right-0 w-48 bg-white mt-1 rounded-md shadow-lg z-10"
+                      onMouseLeave={() => setIsDropdownOpen(false)}
+                    >
+                      <div className="absolute h-3 w-full top-[12px]"></div>
+                      <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        Your Profile
+                      </Link>
+                      <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        Manage Articles
+                      </Link>
+                      <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                        Signout
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </>
+            ) : (
+              /* buttons */
+              <div className=" hidden sm:flex sm:items-center sm:space-x-4">
+                <Link
+                  to="/signin"
+                  className="inline-flex items-center justify-center px-4 py-2 border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+                >
+                  Sign In
+                </Link>
+                <Link
+                  to="/signup"
+                  className="hidden sm:inline-flex items-center justify-center px-4 py-2 text-sm font-medium rounded-md text-orange-600 bg-white border border-orange-600 hover:bg-orange-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+                >
+                  Sign Up
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
