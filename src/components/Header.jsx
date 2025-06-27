c14173def3e5451470826b6b058c56d72e910b27
Ibrahiim10 <ustadibrahim00@gmail.com>
2025-06-27 18:40:10 +0300

    Setting Up Mobile Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router';
 import { FaUser } from 'react-icons/fa';
+import { CiMenuBurger } from 'react-icons/ci';
+import { IoMdClose } from 'react-icons/io';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,6 +14,7 @@ const Header = () => {
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* right and left  humbugger*/}
         <div className="flex justify-between h-16">
           {/* left */}
 
@@ -121,8 +124,92 @@ const Header = () => {
               </div>
             )}
           </div>
+
+          {/* Humbugger */}
+
+          <div className="-mr-2 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="inline-flex items-center justify-center rounded-md text-gray-400 "
+            >
+              {isMenuOpen ? (
+                <CiMenuBurger className="block w-6 h-6" />
+              ) : (
+                <IoMdClose className="block w-6 h-6" />
+              )}
+            </button>
+          </div>
         </div>
       </div>
+
+      {/* mobile menu */}
+
+      {isMenuOpen && (
+        <div className="sm:hidden py-4">
+          <div className="pt-2 pb-3 space-y-1">
+            <Link
+              to="/home"
+              className="block pl-3 pr-4 py-2 border-l-4 border-orange-500 text-base font-medium text-orange-700 bg-orange-50"
+            >
+              Home
+            </Link>
+            <Link
+              to="/articles"
+              className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-100 hover:border-gray-300 hover:text-gray-800"
+            >
+              Articles
+            </Link>
+          </div>
+
+          {/* if is login  */}
+          {isLoggedIn && (
+            <>
+              <Link
+                to="/editor"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Write
+              </Link>
+              <Link
+                to="/manage-article"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                My Article
+              </Link>
+              <Link
+                to="/profile"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Profile
+              </Link>
+              <button
+                // onClick={logout}
+                className="block w-full text-left pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Sign Out
+              </button>
+            </>
+          )}
+
+          {/* if is not login  */}
+          {!isLoggedIn && (
+            <>
+              <Link
+                to="/signin"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Sign In
+              </Link>
+              <Link
+                to="/signup"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
+      )}
     </header>
   );
 };
